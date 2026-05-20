9a024f927dbb105a6c1302407230c509a8f334d5
Dhruv-Gupta0506 <dhruvgupta0506@gmail.com>
2026-05-20 11:47:42 +0530

    initial setup of LedgerX

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const app=require('./src/app');
+const connectDB=require('./src/db/db');
+
+const PORT=process.env.PORT || 3000;
+
+const startServer=async() =>{
+    try{
+        await connectDB();
+        console.log("Database connected");
+
+        app.listen(PORT,()=>{
+            console.log(`server is running on ${PORT}`);
+        });
+    } catch (error){
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
